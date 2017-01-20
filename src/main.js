7a32e0525fd006d10202d8bb7a6f6c16ea953e4e
Kegan Maher <kegan.maher@smgov.net>
2017-01-19 17:04:24 -0800

    use ngTable to simplify

--- src/main.js
+++ src/main.js
@@ -2,29 +2,23 @@
   "use strict";
 
   angular
-    .module("seismic-retrofit", [])
+    .module("seismic-retrofit", ["ngTable"])
     .component("seismicRetrofit", {
-      controller: ["$http", "filterFilter", seismicRetrofit],
+      controller: ["$http", "NgTableParams", seismicRetrofit],
       templateUrl: "seismic-retrofit.html"
     });
 
-    function seismicRetrofit ($http, filter) {
+    function seismicRetrofit ($http, NgTableParams) {
       var ctrl = this;
 
       ctrl.$onInit = function () {
-        ctrl.q = "";
-        ctrl.results = [];
-
         $http.get("data.json", { cache: true }).then(function (results) {
           ctrl.data = results.data;
+          ctrl.tableParams = new NgTableParams(
+            { sorting: { type: "asc" }, count: ctrl.data.length },
+            { dataset: ctrl.data, counts: [] }
+          );
         });
-      };
-
-      ctrl.search = function () {
-        var results = [];
-        results = results.concat(filter(ctrl.data, { address: ctrl.q }));
-        results = results.concat(filter(ctrl.data, { apn: ctrl.q }));
-        ctrl.results = results;
       };
     }
 })();
